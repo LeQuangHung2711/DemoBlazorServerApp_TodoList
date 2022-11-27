7a9b8f597f7477ef7fa42b2f9edb07b1c6292c5d
LeQuangHung2711 <hungquang345@gmail.com>
2022-11-27 17:05:16 +0700

    Demo Blazor Server App - Todo List

---
+++ DemoBlazorSeverApp_TodoList/Data/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoBlazorSeverApp_TodoList.Data
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public string Note { get; set; }
+    }
+}
